8dbd330f0f3b5a80fa0b2d0cb678fc69a6da4785
ankitatembhurne <107808546+ankitatembhurne@users.noreply.github.com>
2022-07-31 13:56:03 +0530

    Add files via upload

---
+++ Assignment2/Assignment2/src/com/ankita/driver/Driver.java
@@ -0,0 +1,21 @@
+package com.ankita.driver;
+import java.util.Scanner;
+import com.ankita.service.*;
+public class Driver {
+
+			public static void main(String[] args) {
+				System.out.println("Enter the total no of floors in the building : ");
+				try (Scanner sc = new Scanner(System.in)) {
+					int numberOfFloors = sc.nextInt();
+					int[] listOfFloors = new int[numberOfFloors];
+					for (int i = 0; i < numberOfFloors; i++) {
+						System.out.println("Enter the floor size given on day : " + (i + 1));
+						int floorSize = sc.nextInt();
+						listOfFloors[i] = floorSize;
+					}
+					Skyscraper.calculateFloors(listOfFloors);
+				}
+			}
+
+		}
+
